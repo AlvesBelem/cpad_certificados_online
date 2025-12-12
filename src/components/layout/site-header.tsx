e172195675283a3f7ce6f17f3c293696c8bb019a
AlvesBelem <marcelocartoes.pa@gmail.com>
2025-12-12 14:28:15 -0300

    fix: import useState in site header

--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { LogOut, ShoppingCart } from "lucide-react";
